49a940e6a4a70cd4e7e80605d602e203969cf108
organizejs <organizejs@gmail.com>
2019-10-16 18:48:59 +0000

    website update

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,1 +1,13 @@
+console.log("Lets save this planet.")
 
+$(document).ready(function() {
+  var license_url = "https://raw.githubusercontent.com/climate-strike/license/master/LICENSE"
+  if ( window.location.pathname == '/' ){
+    $.ajax({
+      url: license_url,
+      success: function(data) { 
+        $("code").html(data.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
+      } 
+    });
+  }
+});
